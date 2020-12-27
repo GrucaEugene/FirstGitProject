45cde4b4881957e8ec9ab745167592ef8a977d14
GrucaEugene <evheniihrutsa@gmail.com>
2020-12-27 15:53:10 +0200

    First test commit

---
+++ src/com/technical/firstGitProject/FirstGitProject.java
@@ -0,0 +1,12 @@
+package com.technical.firstGitProject;
+
+public class FirstGitProject {
+
+    /*** Выполнить любое из предидущих заданий с использованием гит
+     Приложить ссылку на репозиторий
+     Важно: не менее 5ти коммитов (слишком много тоже не надо)*/
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
